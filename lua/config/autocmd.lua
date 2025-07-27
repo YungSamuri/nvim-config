62c7cad3cf011f1c5bf9be626968aad089251e61
Hyrum Smith <hyrum.d.smith@gmail.com>
2025-07-26 21:46:49 -0600

    excluded a few more files from the treesitter autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,8 +1,18 @@
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { '*' },
   callback = function()
-    local unallowed = { TelescopePreview = true, TelescopePrompt = true, TelescopeResults = true, NvimTree = true,
-      ['blink-cmp-documentation'] = true, ['blink-cmp-menu'] = true, }
+    local unallowed = {
+      TelescopePreview = true,
+      TelescopePrompt = true,
+      TelescopeResults = true,
+      NvimTree = true,
+      ['blink-cmp-documentation'] = true,
+      ['blink-cmp-menu'] = true,
+      lazy = true,
+      lazy_backdrop = true,
+      mason = true,
+      mason_backdrop = true,
+    }
     if (unallowed[vim.bo.filetype]) then
       return
     end
