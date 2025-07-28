be81e8384f003f2708792db5572e0bdfff2aca31
Hyrum Smith <hdsmith@byu.edu>
2025-07-28 13:07:58 -0600

    added vue back since it's for alpine js

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     require('nvim-treesitter').install({
       'gdscript', 'godot_resource', 'gdshader', 'c_sharp', 'lua', 'c', 'vim', 'vimdoc', 'markdown', 'javascript', 'html',
-      'css', 'glimmer', 'typescript', 'sql', 'json', 'yaml', 'dockerfile', 'markdown_inline', 'bash',
+      'css', 'glimmer', 'typescript', 'sql', 'json', 'yaml', 'dockerfile', 'markdown_inline', 'bash', 'vue',
     })
   end,
 }
