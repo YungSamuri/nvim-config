e0f99a13448d58f8f88580600a4e0fcd81b31619
Hyrum Smith <hdsmith@byu.edu>
2025-07-29 13:51:10 -0600

    fixed the injection query to highlight sql better in bash scripts

--- after/queries/bash/injections.scm
+++ after/queries/bash/injections.scm
@@ -5,7 +5,7 @@
           (word))
   argument: (word) @query
   (#eq? @query "--query")
-  argument: (string
-              (string_content) @injection.content
-            )
+  argument: (string) @injection.content
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
   (#set! injection.language "sql"))
