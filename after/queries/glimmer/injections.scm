b95cbbaa6cec2a686787d3d822e1c8ba9a0964ba
Hyrum Smith <hdsmith@byu.edu>
2025-07-29 13:52:31 -0600

    an attempt at injecting css and javascript into glimmer (unsuccessful)

--- after/queries/glimmer/injections.scm
+++ after/queries/glimmer/injections.scm
@@ -1,10 +1,27 @@
 ;; extends
 
 ; AlpineJS attributes
-(attribute_node
-  (attribute_name) @_attr
-    (#lua-match? @_attr "^x%-%l")
-    (concat_statement) @injection.content 
-    (#offset! @injection.content 0 1 0 -1)
-    (#set! injection.include-children)
-    (#set! injection.language "javascript"))
+(attribute_name) @_attr
+(#lua-match? @_attr "^x%-%l")
+_ @injection.content 
+(#offset! @injection.content 0 1 0 -1)
+(#set! injection.include-children)
+(#set! injection.language "javascript")
+
+; <script> tags
+((element_node
+  (element_node_start
+    (tag_name) @_tag_name
+    (#eq? @_tag_name "script"))) @injection.content
+  (#offset! @injection.content 0 8 0 -9)
+  (#set! injection.language "javascript")
+  (#set! injection.include-children))
+
+; <style> tags
+((element_node
+  (element_node_start
+    (tag_name) @_tag_name
+    (#eq? @_tag_name "style"))
+  (text_node) @injection.content)
+  (#set! injection.language "css")
+  (#set! injection.include-children))
