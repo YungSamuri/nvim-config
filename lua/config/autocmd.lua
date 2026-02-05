76ed83319832ac36f3c686e95141d01215a5af8d
Hyrum Smith <hdsmith@byu.edu>
2026-02-05 10:20:47 -0700

    enable folds

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -17,6 +17,8 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.treesitter.start()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
   end,
 })
 
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,70 +2,70 @@
 -- General
 -----------------------------------------------------------
 -- Set leader key to space
-vim.g.mapleader = ' '
+vim.g.mapleader          = ' '
 -- Set leader key to space
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader     = ' '
 
 -- Number of spaces a tab represents
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop          = 2
+vim.opt.softtabstop      = 2
 
 -- Use appropriate when using indent command
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
+vim.opt.expandtab        = true
+vim.opt.shiftwidth       = 2
 
 -- Indenting correctly after { etc
-vim.opt.smartindent = true
+vim.opt.smartindent      = true
 
 -- Copy indent from current line when starting new line
-vim.opt.autoindent = true
+vim.opt.autoindent       = true
 
 -- Speeds up plugin wait time
-vim.opt.updatetime = 50
+vim.opt.updatetime       = 50
 
 -- Persistant undo file history
-vim.opt.undofile = true
+vim.opt.undofile         = true
 
 -- Disable default file explorer
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Notify neovim that a nerd font is installed
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font     = true
 
 -----------------------------------------------------------
 -- UI Config
 -----------------------------------------------------------
 -- Enable line numbers
-vim.opt.nu = true
+vim.opt.nu               = true
 
 -- Enable relative line numbers
-vim.opt.rnu = true
+vim.opt.rnu              = true
 
 -- Put relative and regular line number in the same column
-vim.opt.numberwidth = 1
+vim.opt.numberwidth      = 1
 
 -- Enable cursor line highlight
-vim.opt.cursorline = true
+vim.opt.cursorline       = true
 
 -- Better completion experience
-vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.completeopt      = { 'menuone', 'noselect' }
 
 -- Enable 24-bit color
-vim.opt.termguicolors = true
+vim.opt.termguicolors    = true
 
 -- Enable the sign column to prevent the screen from jumping
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn       = 'yes'
 
 -- Always keep 8 lines above/below cursor unless at start/end of file
-vim.opt.scrolloff = 8
+vim.opt.scrolloff        = 10
 
 -- Better splitting
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.opt.splitbelow       = true
+vim.opt.splitright       = true
 
 -- Faster scrolling
-vim.opt.lazyredraw = true
+vim.opt.lazyredraw       = true
 
 -- Highlight yank
 vim.api.nvim_create_autocmd('textyankpost', {
@@ -76,6 +76,9 @@ vim.api.nvim_create_autocmd('textyankpost', {
     vim.highlight.on_yank({ timeout = 200, visual = true })
   end,
 })
+
+-- Show folds on the left
+vim.opt.foldcolumn = 'auto:9'
 
 -----------------------------------------------------------
 -- Search Config
