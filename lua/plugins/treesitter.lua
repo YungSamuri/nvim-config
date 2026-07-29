ab08d71818f1ac7803b5c8f94d0e37ed087f2f75
Hyrum Smith <hyrum.d.smith@gmail.com>
2026-07-28 21:32:46 -0600

    added flutter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,10 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter').install({
-      'gdscript', 'godot_resource', 'gdshader', 'c_sharp', 'lua', 'c', 'vim', 'vimdoc', 'markdown', 'javascript',
-      'css', 'typescript', 'sql', 'json', 'yaml', 'dockerfile', 'markdown_inline', 'bash', 'vue', 'gotmpl',
-      'python', 'csv', 'superhtml', 'jsdoc', 'zsh', 'go', 'gomod', 'gosum', 'glimmer', 'kotlin', 'java', 'html'
+      'gdscript', 'godot_resource', 'gdshader', 'c_sharp', 'lua', 'c', 'vim', 'vimdoc', 'markdown',
+      'javascript', 'css', 'typescript', 'sql', 'json', 'yaml', 'dockerfile', 'markdown_inline',
+      'bash', 'vue', 'gotmpl', 'python', 'csv', 'superhtml', 'jsdoc', 'zsh', 'go', 'gomod', 'gosum', 'glimmer',
+      'kotlin', 'java', 'dart', 'html',
     })
   end,
 }
