129bfc9bca6b36d54c1248ea33efb69dbf5b2691
Hyrum Smith <hdsmith@byu.edu>
2026-08-25 16:36:33 -0600

    ensure dart treesitter is installed, removed commented out code, disable gotmpl lsp highlights

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -13,12 +13,10 @@ vim.api.nvim_create_autocmd('User', {
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'bash', 'zsh', 'lua', 'javascript', 'typescript', 'css', 'html', 'sql', 'json', 'yaml', 'dockerfile', 'python', 'go', 'gomod', 'gosum', 'glimmer', 'superhtml', 'kotlin', 'java', 'gotmpl', 'tmpl' },
+  pattern = { 'bash', 'zsh', 'lua', 'javascript', 'typescript', 'css', 'html', 'sql', 'json', 'yaml', 'dockerfile', 'python', 'go', 'gomod', 'gosum', 'glimmer', 'superhtml', 'kotlin', 'java', 'gotmpl', 'tmpl', 'dart' },
   callback = function()
     vim.treesitter.start()
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    -- vim.wo[0][0].foldmethod = 'expr'
   end,
 })
 
@@ -50,6 +48,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = {'*.tmpl'},
   callback = function()
     vim.bo.filetype = "gotmpl"
+    vim.api.nvim_set_hl(0, '@lsp.type.macro.gotmpl', {})
   end,
 })
 
@@ -60,44 +59,9 @@ vim.filetype.add({
 })
 
 
--- vim.api.nvim_create_autocmd('LspAttach', {
---   callback = function(args)
---     local client = vim.lsp.get_client_by_id(args.data.client_id)
---
---     if not client then
---       return
---     end
---
---     if client.name == 'biome' then
---       vim.api.nvim_create_autocmd('BufWritePre', {
---         group = vim.api.nvim_create_augroup('BiomeFixAll', { clear = true }),
---         callback = function()
---           vim.lsp.buf.code_action({
---             context = {
---               only = { 'source.fixAll.biome' },
---               diagnostics = {},
---             },
---             apply = true,
---           })
---         end,
---       })
---     end
---   end,
--- })
-
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   callback = function()
     -- vim.lsp.buf.format({ async = true })
     require('lint').try_lint()
   end
 })
-
--- Filetype tab size overrides
---vim.api.nvim_create_autocmd('FileType', {
---  pattern = "superhtml",
---  callback = function()
---    vim.opt_local.tabstop = 4
---    vim.opt_local.shiftwidth = 4
---    vim.opt_local.expandtab = true
---  end,
---})
